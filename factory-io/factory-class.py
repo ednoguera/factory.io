968c697be127ab46457e03b66f23e3be89ec8f86
Edson Silva <edson@purespectrum.com>
2021-07-06 19:25:50 -0300

    first commit

---
+++ factory-io/factory-class.py
@@ -0,0 +1,12 @@
+
+
+class Factory:
+    
+    def __init__(self, name, type, funds, productList, materialList) -> None:
+        self.name = name
+        self.type = type
+        self.funds = funds
+        self.productList = productList
+        self.materialList = materialList
+    
+    
